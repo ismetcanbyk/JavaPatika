55e617a6698962b586eab0bf8fb8a76f5b701f17
ismetcanbyk <ismetcanbyk@gmail.com>
2022-09-23 11:11:00 +0300

    Kdv hesaplama uygulaması.

---
+++ KdvHesaplama.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;;
+
+public class KdvHesaplama {
+    public static void main(String[] args){
+        int tutar;
+        float kdv;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Tutar Gir : ");
+        tutar = inp.nextInt();
+        kdv = tutar<=1000 ? (tutar*18)/100f : (tutar*8)/100f;
+
+        System.out.println(tutar+kdv);
+    }
+}
