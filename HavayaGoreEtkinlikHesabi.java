aa2401f032517d9de2eeea0977742566eafcc5b5
ismetcanbyk <ismetcanbyk@gmail.com>
2022-09-26 11:23:33 +0300

    Hava durumuna göre etkinlik seçme uygulaması

---
+++ HavayaGoreEtkinlikHesabi.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class HavayaGoreEtkinlikHesabi {
+    public static void main(String[] args) {
+        int sicaklik;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Hava Sıcaklığı : ");
+        sicaklik = inp.nextInt();
+
+        if (sicaklik < 5) {
+            System.out.println("Kayak");
+        } else if (sicaklik == 15) {
+            System.out.println("Sinema Ve Piknik");
+        } else if (sicaklik >= 5 && sicaklik <= 15) {
+            System.out.println("Sinema");
+        } else if (sicaklik >= 15 && sicaklik <= 25) {
+            System.out.println("Piknik");
+        } else if (sicaklik > 25) {
+            System.out.println("Yüzme");
+        }
+
+    }
+}
--- SinifGecme.java
+++ SinifGecme.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import javax.lang.model.util.ElementScanner14;
-
 public class SinifGecme {
     public static void main(String[] args) {
         int mat, fiz, turkce, kim, muzik;
