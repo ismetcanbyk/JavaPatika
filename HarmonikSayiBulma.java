be8a9845f3a061ee998096abf3c003aef16e23f2
ismetcanbyk <ismetcanbyk@gmail.com>
2022-09-28 11:33:17 +0300

    Harmonik sayı bulma uygulaması.

--- HarmonikSayiBulma.java
+++ HarmonikSayiBulma.java
@@ -6,12 +6,12 @@ public class HarmonikSayiBulma {
         double result = 0.0;
         Scanner inp = new Scanner(System.in);
 
-        System.out.print("Sayı girin : ");
+        System.out.print("\nSayı girin : ");
         n = inp.nextInt();
 
         for (double i = 1; i <= n; i++) {
             result += (1/i);
         }
-        System.out.println(result);
+        System.out.println("\n Harmonik değer : "+result);
     }
 }
