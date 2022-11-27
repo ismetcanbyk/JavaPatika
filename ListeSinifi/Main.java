1eefdc751a4c846afbde4af7ef74b243d9261e87
ismetcanbyk <ismetcanbyk@gmail.com>
2022-11-27 22:52:54 +0300

    Liste sınıfı yapıldı

---
+++ ListeSinifi/Main.java
@@ -0,0 +1,21 @@
+package ListeSinifi;
+
+public class Main {
+    public static void main(String[] args) {
+        
+
+        MyList<Integer> list = new MyList<>();
+
+        list.add(12);
+        list.add(122);
+        list.add(123);
+        list.add(142);
+        list.add(152);
+        list.add(2);
+
+        
+
+        
+
+    }
+}
